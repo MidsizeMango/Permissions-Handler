e2a6deb79f9196a1c2c27df0062eead88e22e3b4
Prasad Shirvandkar <prasadshirvandkar1996@gmail.com>
2017-07-17 00:44:27 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/prasad/permissionshelpersample/MainActivity.kt
+++ app/src/main/java/com/example/prasad/permissionshelpersample/MainActivity.kt
@@ -14,27 +14,32 @@ class MainActivity : PermissionHelperActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val checkButton = findViewById(R.id.check_button) as Button
-        checkButton.setOnClickListener {
-            requestPermission(Permissions.CAMERA, object: PermissionResult{
-                override fun permissionGranted() {
-                    toast("Permissions Granted")
-                }
-
-                override fun permissionDenied() {
-                    toast("Permissions Denied")
-                }
-
-                override fun permissionDeniedPermanently() {
-                    //openSettingsPermission(applicationContext)
-                    toast("Allow Permissions from App Settings")
-                }
-            })
-        }
-
+        
         val permissions = arrayOf(Permissions.ACCESS_COARSE_LOCATION,Permissions.READ_EXTERNAL_STORAGE,
                 Permissions.WRITE_EXTERNAL_STORAGE,Permissions.READ_CONTACTS)
+        
+        val granted1: Boolean = isPermissionGranted(this, Permissions.CAMERA)
+        val granted2: Boolean = arePermissionsGranted(this, permissions)
+        
+        val checkButton = findViewById(R.id.check_button) as Button
+        checkButton.setOnClickListener {
+            if(!granted1) {
+                requestPermission(Permissions.CAMERA, object : PermissionResult {
+                    override fun permissionGranted() {
+                        toast("Permissions Granted")
+                    }
+
+                    override fun permissionDenied() {
+                        toast("Permissions Denied")
+                    }
+
+                    override fun permissionDeniedPermanently() {
+                        //openSettingsPermission(applicationContext)
+                        toast("Allow Permissions from App Settings")
+                    }
+                })
+            }
+        }
 
         requestMultiplePermissions(permissions, object: PermissionResult {
             override fun permissionGranted() {
